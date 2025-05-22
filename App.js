33fdb7504bf67911293a146767ae668955cc178e
Mikolaj775 <kontonowe380@gmail.com>
2025-05-22 15:44:44 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1649,6 +1649,8 @@ setWalls([
 
   return (
     <div className="App" style={{
+      width: '100vw',
+      height: '100vh',
       margin:0,
       padding:0,
       backgroundColor: elements2 == "visible" ? "#d4c4ca" : "#2b3b35",
